61d5b02c6f98ede26de432c7f0c62f15d05b3d4c
wang7211401 <wang7211401@163.com>
2020-05-20 17:09:11 +0800

    update

--- models/voteType.js
+++ models/voteType.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose")
-const Schema =  mongoose.Schema
+const Schema = mongoose.Schema
 const schema = new Schema({
-  voteId:{ type: Schema.Types.ObjectId, ref: "Vote" },
-  ItemId:{type: Schema.Types.ObjectId, ref: "Item"},
+  voteId: { type: Schema.Types.ObjectId, ref: "Vote" },
   title: { type: String },
   create_time: {
     type: Date,
