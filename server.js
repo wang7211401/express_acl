9cf37bad1b4f0b221b73a5ebcd961e93087c249c
wang7211401 <wang7211401@163.com>
2020-06-28 16:28:52 +0800

    更新端口号

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ require("./plugins/db")(app).then((acl) => {
   require("./routes/admin")(app, acl)
 })
 
-app.listen(3000, () => {
-  console.log("listening on port 3000")
+app.listen(3002, () => {
+  console.log("listening on port 3002")
 })
